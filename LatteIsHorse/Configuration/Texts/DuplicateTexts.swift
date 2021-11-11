61b54d6837d9068feb35023803ebb23f24f9212e
iammiori <mioriii@naver.com>
2021-11-12 03:36:34 +0900

    [#9] feat : 북마크 뷰컨에 필요한 텍스트 모델 형성

---
+++ LatteIsHorse/Configuration/Texts/DuplicateTexts.swift
@@ -0,0 +1,13 @@
+//
+//  DuplicateTexts.swift
+//  LatteIsHorse
+//
+//  Created by miori Lee on 2021/11/12.
+//
+
+import Foundation
+
+struct DuplicateTexts {
+    static let noQuiz = "저장한 퀴즈가 없습니다."
+    static let noAlarm = "알림 내역이 없습니다."
+}
