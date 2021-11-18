648251687fe2ac33f3864bd9d478b81c10eacd5d
iammiori <immioriii@gmail.com>
2021-11-18 22:14:56 +0900

    [#2] feat : uiview extension 추가 : bottom line 추가

---
+++ LatteIsHorse/Configuration/Extension/UIView.swift
@@ -0,0 +1,36 @@
+//
+//  UIView.swift
+//  LatteIsHorse
+//
+//  Created by miori Lee on 2021/11/12.
+//
+
+import UIKit
+
+enum LinePosition {
+    case top
+    case bottom
+}
+
+extension UIView {
+
+    func addBottomorTopBorder(position: LinePosition, color: UIColor, width: Double) {
+        let lineView = UIView()
+        lineView.backgroundColor = color
+        lineView.translatesAutoresizingMaskIntoConstraints = false // This is important!
+        self.addSubview(lineView)
+
+        let metrics = ["width" : NSNumber(value: width)]
+        let views = ["lineView" : lineView]
+        self.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[lineView]|", options:NSLayoutConstraint.FormatOptions(rawValue: 0), metrics:metrics, views:views))
+
+        switch position {
+        case .top:
+            self.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[lineView(width)]", options:NSLayoutConstraint.FormatOptions(rawValue: 0), metrics:metrics, views:views))
+            break
+        case .bottom:
+            self.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:[lineView(width)]|", options:NSLayoutConstraint.FormatOptions(rawValue: 0), metrics:metrics, views:views))
+            break
+        }
+    }
+}
